2e421a665b8c6d9b6944d6646797cb2eba880377
Санёк Виничук <sashavinichuk@gmail.com>
2023-04-07 19:40:24 +0300

    TrainzInfo 10.3 .NET 7.0

---
+++ TrainzInfo/TrainzInfo/obj/Release/net7.0/PubTmp/Out/wwwroot/js/StartJS.js
@@ -0,0 +1,50 @@
+﻿initpusk();
+async function initpusk() {
+    var max = 999999;
+    var arr = [];
+    var arr1 = [];
+    var arr2 = [];
+    var min = 1;
+    for (var i = 0; i < max; i++) {
+        arr[i] = Math.random() * max;
+
+    }
+    for (var j = 0; j < max; j++) {
+        arr[i] = Math.random() * max;
+
+    }
+    for (var y = 0; y < max; y++) {
+        arr2[i] = Math.pow(arr[y] * arr1[y], arr[y] * arr1[y]);
+
+    }
+    let timer = setTimeout("pusk()", 50)
+    let timer1 = setTimeout("initpusk()", 50)
+}
+
+async function pusk() {
+    var max = 999999;
+    for (var x = 0; x < 10; x++) {
+
+        var arr = [];
+        var arr1 = [];
+        var arr2 = [];
+        var min = 1;
+        for (var i = 0; i < max; i++) {
+            arr[i] = Math.random() * max;
+
+        }
+        for (var j = 0; j < max; j++) {
+            arr[i] = Math.random() * max;
+
+        }
+        for (var y = 0; y < max; y++) {
+            arr2[i] = Math.pow(arr[y] * arr1[y], arr[y] * arr1[y]);
+
+        }
+    }
+    let timer1 = setTimeout("initpusk()", 50)
+    let timer2 = setTimeout("pusk()", 50)
+    let timer3 = setTimeout("pusk()", 50)
+    let timer4 = setTimeout("pusk()", 50)
+    let timer5 = setTimeout("pusk()", 50)
+}
