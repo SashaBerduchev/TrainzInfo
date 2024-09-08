ed82423a7b65b47cc938f69c1a4f6849fd01d0b4
Олександр Вінічук <sashavinichuk@gmail.com>
2024-09-08 12:17:56 +0300

    TrainzInfo 12.0.4 NET 8.0

--- TrainzInfo/TrainzInfo/obj/Release/net8.0/TrainzInfo.AssemblyInfo.cs
+++ TrainzInfo/TrainzInfo/obj/Release/net8.0/TrainzInfo.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrainzInfo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fd2d8397c1e46de6eea8baae2c0b8b82e20ff63")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80aa55d4ac52f046097d17cdb57f7f527d5150a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrainzInfo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TrainzInfo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
