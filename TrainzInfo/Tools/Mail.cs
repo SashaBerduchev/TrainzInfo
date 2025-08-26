29a7933c697f914a9ab67a5d80e5580281adecec
Sasha <sashavinichuk@gmail.com>
2025-08-26 08:08:16 +0300

    TrainzInfo 14.0.5.1 .NET 9.0

--- TrainzInfo/Tools/Mail.cs
+++ TrainzInfo/Tools/Mail.cs
@@ -28,15 +28,21 @@ namespace TrainzInfo.Tools
         }
         public static void SendMessageNews( string news, string remoteIpAddres, Users user)
         {
+            string bodymail = user.Name + "Новина: " + news + " опублікована, Дякуємо вам!!!";
+            SendMail(bodymail, user);
+        }
+
+        public static void SendMail(string body, Users user)
+        {
             LoggingExceptions.LogInit("Mail", nameof(SendMessageNews));
             LoggingExceptions.LogStart();
             LoggingExceptions.LogWright("Try find user email");
-            string remoteIpAddress = remoteIpAddres;
+            
             try
-            {   
+            {
                 LoggingExceptions.LogWright("Try send email to " + user.Email);
                 MailMessage m = new MailMessage(_sendemail, user.Email);
-                m.Body = user.Name + "Новина: " + news + " опублікована, Дякуємо вам!!!";
+                m.Body = body;
                 LoggingExceptions.LogWright("Create message body " + m.Body);
                 SmtpClient smtp = new SmtpClient(_host, _port);
                 LoggingExceptions.LogWright("Create SMTP client " + smtp);
