1111ed49a7853a0f1fce95368e2d907be6f1ae91
Sasha <sashavinichuk@gmail.com>
2025-09-06 09:53:00 +0300

    TrainzInfo 15.0.1 .NET 9.0

---
+++ TrainzInfo/Tools/EditProfileViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TrainzInfo.Tools
+{
+    public class EditProfileViewModel
+    {
+        [Required]
+        [Display(Name = "Логін")]
+        public string UserName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+        [DataType(DataType.Password)]
+        [Display(Name = "Старий пароль")]
+        public string OldPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Новий пароль")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Підтвердження нового пароля")]
+        [Compare("NewPassword", ErrorMessage = "Паролі не співпадають")]
+        public string ConfirmNewPassword { get; set; }
+    }
+
+}
