d91f20340734e236b401e879b8bd96c8f447dcc1
Санёк Виничук <alexander.vinichuk@outlook.com>
2025-11-25 14:44:22 +0200

    TrainzInfo 21.0.0.2.NET 10

--- TrainzInfo/Controllers/Api/LocomotivesApiController.cs
+++ TrainzInfo/Controllers/Api/LocomotivesApiController.cs
@@ -43,22 +43,23 @@ namespace TrainzInfo.Controllers.Api
                     .Skip((page - 1) * pageSize)
                     .Take(pageSize)
                     .Select(n => new LocmotiveDTO
-                     {
-                         Id = n.id,
-                         Number = n.Number,
-                         Speed = n.Speed,
-                         Depot = n.DepotList.Name,
+                    {
+                        Id = n.id,
+                        Number = n.Number,
+                        Speed = n.Speed,
+                        Depot = n.DepotList.Name,
                         City = n.DepotList.City.Name,
-                         Oblast = n.DepotList.City.Oblasts.Name,
-                         Filia = n.DepotList.UkrainsRailway.Name,
-                         Seria = n.Locomotive_Series.Seria,
-                         ImgSrc = n.Image != null
+                        Oblast = n.DepotList.City.Oblasts.Name,
+                        Filia = n.DepotList.UkrainsRailway.Name,
+                        Seria = n.Locomotive_Series.Seria,
+                        ImgSrc = n.Image != null
                                 ? $"data:{n.ImageMimeTypeOfData};base64,{Convert.ToBase64String(n.Image)}"
                                 : null,
-                         // або формат за потребою
-                     }).ToListAsync();
+                        // або формат за потребою
+                    }).ToListAsync();
                 return Ok(locoDTO);
-            } catch (Exception ex)
+            }
+            catch (Exception ex)
             {
                 LoggingExceptions.AddException(ex.ToString());
                 LoggingExceptions.Wright(ex.ToString());
@@ -71,9 +72,105 @@ namespace TrainzInfo.Controllers.Api
             }
         }
 
-        public IActionResult Index()
+        [HttpGet("getseries")]
+        public async Task<ActionResult<List<string>>> GetSeries()
         {
-            return View();
+            try
+            {
+                LoggingExceptions.Init("LocomotivesApiController", "GetSeries");
+                LoggingExceptions.Start();
+                LoggingExceptions.Wright("Start Get GetSeries");
+                var series = await _context.Locomotive_Series
+                    .Select(x => x.Seria)
+                    .ToListAsync();
+                return Ok(series);
+            }
+            catch (Exception ex)
+            {
+                LoggingExceptions.AddException(ex.ToString());
+                LoggingExceptions.Wright(ex.ToString());
+                return BadRequest();
+                throw;
+            }
+            finally
+            {
+                LoggingExceptions.Finish();
+            }
+        }
+
+        [HttpGet("getdepots")]
+        public async Task<ActionResult<List<string>>> GetDepots()
+        {
+            try
+            {
+                LoggingExceptions.Init("LocomotivesApiController", "GetDepots");
+                LoggingExceptions.Start();
+                LoggingExceptions.Wright("Start Get GetDepots");
+                var depots = await _context.Depots
+                    .Select(x => x.Name)
+                    .ToListAsync();
+                return Ok(depots);
+            }
+            catch (Exception ex)
+            {
+                LoggingExceptions.AddException(ex.ToString());
+                LoggingExceptions.Wright(ex.ToString());
+                return BadRequest();
+                throw;
+            }
+            finally
+            {
+                LoggingExceptions.Finish();
+            }
+        }
+
+        [HttpPost("create")]
+        public async Task<ActionResult> CreateLocomotive([FromBody] LocmotiveDTO locomotiveDTO)
+        {
+            try
+            {
+                LoggingExceptions.Init("LocomotivesApiController", "CreateLocomotive");
+                LoggingExceptions.Start();
+                LoggingExceptions.Wright("Start Post CreateLocomotive");
+                var locomotive = new Locomotive
+                {
+                    Number = locomotiveDTO.Number,
+                    Speed = locomotiveDTO.Speed,
+                    // Assuming you have logic to fetch related entities based on names
+                    Locomotive_Series = await _context.Locomotive_Series
+                        .FirstOrDefaultAsync(s => s.Seria == locomotiveDTO.Seria),
+                    DepotList = await _context.Depots
+                        .FirstOrDefaultAsync(d => d.Name == locomotiveDTO.Depot),
+                    // Image handling can be added here if needed
+                    Seria = locomotiveDTO.Seria
+                };
+
+                LoggingExceptions.Wright("Parse image");
+                if (!string.IsNullOrEmpty(locomotiveDTO.ImgSrc))
+                {
+                    // Якщо рядок має формат "data:image/png;base64,....", треба відокремити сам base64
+                    var base64Data = locomotiveDTO.ImgSrc.Contains(",")
+                        ? locomotiveDTO.ImgSrc.Split(',')[1]
+                        : locomotiveDTO.ImgSrc;
+
+                    byte[] imageBytes = Convert.FromBase64String(base64Data);
+                    locomotive.Image = imageBytes;
+                }
+                _context.Locomotives.Add(locomotive);
+                await _context.SaveChangesAsync();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                LoggingExceptions.AddException(ex.ToString());
+                LoggingExceptions.Wright(ex.ToString());
+                return BadRequest(ex.ToString());
+                throw;
+            }
+            finally
+            {
+                LoggingExceptions.Finish();
+            }
         }
     }
 }
