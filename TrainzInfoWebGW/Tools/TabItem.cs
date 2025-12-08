e09a0e541ca683dd88479e241385a4f52a27710d
Санёк Виничук <alexander.vinichuk@outlook.com>
2025-12-08 19:06:06 +0200

    Revert "TrainzInfo 24.0.0.3 .NET 10"
    This reverts commit 1034efca70a54876ecb37df7db665dff7b9ca4ca.

--- TrainzInfoWebGW/Tools/TabItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TrainzInfoWebGW.Tools
-{
-    public class TabItem
-    {
-        public string Id { get; set; } = Guid.NewGuid().ToString();
-        public string Title { get; set; } = "";
-        public Type ComponentType { get; set; }
-    }
-}
