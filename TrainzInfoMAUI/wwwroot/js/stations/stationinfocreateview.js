3248b21d4adb32d801857c2ca76ae737f39ace7d
Санёк Виничук <alexander.vinichuk@outlook.com>
2026-01-31 15:53:10 +0200

    "TrainzInfo 27.1.5 .NET 10"

---
+++ TrainzInfoMAUI/wwwroot/js/stations/stationinfocreateview.js
@@ -0,0 +1,14 @@
+﻿window.initCKEditorForStationInfo = (textareaId, dotNetHelper, methodName, initialData) => {
+    const el = document.querySelector(`#${textareaId}`);
+    if (!el) return;
+
+    ClassicEditor
+        .create(el)
+        .then(editor => {
+            if (initialData) editor.setData(initialData); // встановлюємо початкові дані, якщо є
+            editor.model.document.on('change:data', () => {
+                dotNetHelper.invokeMethodAsync(methodName, editor.getData());
+            });
+        })
+        .catch(error => console.error(error));
+};
